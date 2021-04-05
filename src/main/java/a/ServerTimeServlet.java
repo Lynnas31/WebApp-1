f71c2f8300afb58c2444ecc2db5c17305cf6537f
Hien_Nguyen <h.phuong211296@gmail.com>
2021-04-05 14:54:13 +0700

    1

---
+++ src/main/java/a/ServerTimeServlet.java
@@ -0,0 +1,25 @@
+package a;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+@WebServlet(name = "ServerTimeServlet", value = "/")
+public class ServerTimeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        Date today = new Date();
+        writer.println("<h1>Hello world"+today+"</h1>");
+        writer.println("</html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
